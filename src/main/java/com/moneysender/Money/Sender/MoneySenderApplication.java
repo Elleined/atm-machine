ab2b25331c07cda493f9e01f7e510b14a1e6935f
Elleined <demadegu@gmail.com>
2023-06-16 16:55:14 +0800

    Initial commit

---
+++ src/main/java/com/moneysender/Money/Sender/MoneySenderApplication.java
@@ -0,0 +1,13 @@
+package com.moneysender.Money.Sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneySenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneySenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moneysender/Money/Sender/MoneySenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneysender.Money.Sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneySenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
