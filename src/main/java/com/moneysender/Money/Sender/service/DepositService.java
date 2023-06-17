9e40240ca5a508a6848fe9c7fc113a9c75ef9af8
Elleined <demadegu@gmail.com>
2023-06-17 15:30:15 +0800

    Deposit transaction will now be saved every deposit that user make

--- src/main/java/com/moneysender/Money/Sender/service/DepositService.java
+++ src/main/java/com/moneysender/Money/Sender/service/DepositService.java
@@ -1,7 +1,9 @@
 package com.moneysender.Money.Sender.service;
 
 import com.moneysender.Money.Sender.exception.ResourceNotFoundException;
+import com.moneysender.Money.Sender.model.DepositTransaction;
 import com.moneysender.Money.Sender.model.User;
+import com.moneysender.Money.Sender.repository.DepositTransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +20,7 @@ import java.math.BigDecimal;
 public class DepositService {
     private final UserService userService;
     private final ATMValidator atmValidator;
+    private final DepositTransactionRepository depositTransactionRepository;
 
     @Transactional
     public BigDecimal deposit(int userId, @NonNull BigDecimal amount)
@@ -31,7 +36,24 @@ public class DepositService {
         BigDecimal newBalance = user.getBalance().add(amount);
         user.setBalance(newBalance);
         userService.save(user);
+
+        saveDepositTransaction(user, amount);
+
         log.debug("User with id of {} deposited amounting {}.\nOld balance: {}\nNew Balance: {}", userId, amount, oldBalance, newBalance);
         return user.getBalance();
+    }
+
+    @Transactional
+    private void saveDepositTransaction(User user, @NonNull BigDecimal depositedAmount) {
+        String trn = UUID.randomUUID().toString();
+        DepositTransaction depositTransaction = DepositTransaction.builder()
+                .user(user)
+                .amount(depositedAmount)
+                .depositDate(LocalDateTime.now())
+                .trn(trn)
+                .build();
+
+        depositTransactionRepository.save(depositTransaction);
+        log.debug("Deposit transaction saved with trn of {}", trn);
     }
 }
