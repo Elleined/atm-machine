2f9ad1589e4ff513a6921d134c67f8ac77501f91
Elleined <demadegu@gmail.com>
2023-06-17 15:32:08 +0800

    Withdraw transaction will now be saved every withdraw that user make

--- src/main/java/com/moneysender/Money/Sender/service/WithdrawService.java
+++ src/main/java/com/moneysender/Money/Sender/service/WithdrawService.java
@@ -3,7 +3,10 @@ package com.moneysender.Money.Sender.service;
 
 import com.moneysender.Money.Sender.exception.InsufficientFundException;
 import com.moneysender.Money.Sender.exception.ResourceNotFoundException;
+import com.moneysender.Money.Sender.model.DepositTransaction;
 import com.moneysender.Money.Sender.model.User;
+import com.moneysender.Money.Sender.model.WithdrawTransaction;
+import com.moneysender.Money.Sender.repository.WithdrawTransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
@@ -11,6 +14,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public class WithdrawService {
 
     private final UserService userService;
     private final ATMValidator atmValidator;
+    private final WithdrawTransactionRepository withdrawTransactionRepository;
 
     @Transactional
     public BigDecimal withdraw(int userId, @NonNull BigDecimal amount)
@@ -40,7 +46,24 @@ public class WithdrawService {
         BigDecimal newBalance = user.getBalance().subtract(amount);
         user.setBalance(newBalance);
         userService.save(user);
+
+        saveWithdrawTransaction(user, amount);
+
         log.debug("User with id of {} withdraw amounting {}.\nOld balance: {}\nNew Balance: {}", userId, amount, oldBalance, newBalance);
         return user.getBalance();
+    }
+
+    @Transactional
+    private void saveWithdrawTransaction(User user, @NonNull BigDecimal withdrawalAmount) {
+        String trn = UUID.randomUUID().toString();
+        WithdrawTransaction withdrawTransaction = WithdrawTransaction.builder()
+                .user(user)
+                .amount(withdrawalAmount)
+                .withdrawalDate(LocalDateTime.now())
+                .trn(trn)
+                .build();
+
+        withdrawTransactionRepository.save(withdrawTransaction);
+        log.debug("Deposit transaction saved with trn of {}", trn);
     }
 }
