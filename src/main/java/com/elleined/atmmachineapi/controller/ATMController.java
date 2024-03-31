5c8f5334b6de746a0c8c034966bb8115cb6e726e
Elleined <demadegu@gmail.com>
2024-03-31 11:39:00 +0800

    Major Refactoring Completed

--- src/main/java/com/elleined/atmmachineapi/controller/ATMController.java
+++ src/main/java/com/elleined/atmmachineapi/controller/ATMController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("/machine/{currentUserId}")
+@RequestMapping("/users/{currentUserId}")
 @RequiredArgsConstructor
 public class ATMController  {
     private final ATMService atmService;
