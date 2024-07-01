e28755174e72eea4816e4021fda6e1b1a5b3aab2
Elleined <demadegu@gmail.com>
2024-07-01 10:44:33 +0800

    Updated pom.xml and application properties

--- src/main/java/com/elleined/atmmachineapi/ATMMachineApplication.java
+++ src/main/java/com/elleined/atmmachineapi/ATMMachineApplication.java
@@ -2,10 +2,14 @@ package com.elleined.atmmachineapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ATMMachineApplication {
 
 	public static void main(String[] args) {
